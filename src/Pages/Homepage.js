215156b491131d3b21b7241762a89e8f408750f8
mariya-21 <mariyamathew219@gmail.com>
2022-07-27 13:49:16 +0530

    home page modified

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import  Banner  from '../components/Banner/Banner';
 import CoinsTable from '../components/Banner/CoinsTable';
-import CoinPage from './CoinPage';
+// import CoinPage from './CoinPage';
  const Homepage = () => {
     return (
         <div>
